e71a278bf0bae113517a9f8423f9a23644e15438
Daniel Ziegler <dmz@openai.com>
2018-07-10 14:03:02 -0700

    gc

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import gc
 from time import sleep
 from psutil import Process
 from tuple_fun import pytuple, stltuple
@@ -7,9 +8,11 @@ if __name__ == "__main__":
 
     print("pytuple()")
     for i in range(10000000): pytuple()
+    gc.collect()
     print("memory usage:", Process().memory_full_info())
 
     print("stltuple()")
     for i in range(10000000): stltuple()
+    gc.collect()
     print("memory usage:", Process().memory_full_info())
 
